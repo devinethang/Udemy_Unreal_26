daaddb54c7ff3458f5c8669116a0a9d36147337b
Devin Graupmann <devinethang@gmail.com>
2019-09-01 23:46:23 -0700

    added ascii art

--- Udemy_Unreal_26.cpp
+++ Udemy_Unreal_26.cpp
@@ -1,11 +1,29 @@
-// Udemy_Unreal_26.cpp : Defines the entry point for the console application.
+// Udemy_Unreal_24.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
+
+void PrintIntroduction()
+{
+	cout << "This is ascii art." << endl;
+	cout << "H   H EEEEE L     L      OOO       W   W  OOO  RRRR  L     DDDD  !!" << endl;
+	cout << "H   H E     L     L     O   O      W W W O   O R   R L     D   D !!" << endl;
+	cout << "HHHHH EEEEE L     L     O   O      W W W O   O RRRR  L     D   D !!" << endl;
+	cout << "H   H E     L     L     O   O, ,    W W  O   O R   R L     D   D" << endl;
+	cout << "H   H EEEEE LLLLL LLLLL  OOO, ,     W W   OOO  R   R LLLLL DDDD  !!" << endl;
+}
+
+void PlayGame()
+{
+	PrintIntroduction();
+}
 
 int main()
 {
-    return 0;
-}
+	PlayGame();
 
+	return 0;
+}
