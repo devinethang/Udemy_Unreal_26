6ed1211084f980d3ce703bb18ff341e50fea3cd4
Devin Graupmann <devinethang@gmail.com>
2019-09-02 00:05:42 -0700

    Better art

--- Udemy_Unreal_26.cpp
+++ Udemy_Unreal_26.cpp
@@ -8,12 +8,25 @@ using namespace std;
 
 void PrintIntroduction()
 {
-	cout << "This is ascii art." << endl;
-	cout << "H   H EEEEE L     L      OOO       W   W  OOO  RRRR  L     DDDD  !!" << endl;
-	cout << "H   H E     L     L     O   O      W W W O   O R   R L     D   D !!" << endl;
-	cout << "HHHHH EEEEE L     L     O   O      W W W O   O RRRR  L     D   D !!" << endl;
-	cout << "H   H E     L     L     O   O, ,    W W  O   O R   R L     D   D" << endl;
-	cout << "H   H EEEEE LLLLL LLLLL  OOO, ,     W W   OOO  R   R LLLLL DDDD  !!" << endl;
+	cout << "This is ASCII art." << endl;
+	cout << "  ___ ___         .__  .__            __      __            .__       .___" << endl;
+	cout << " /   |   \\   ____ |  | |  |   ____   /  \\    /  \\___________|  |    __| _/" << endl;
+	cout << "/    ~    \\_/ __ \\|  | |  |  /  _ \\  \\   \\/\\/   /  _ \\_  __ \\  |   / __ | " << endl;
+	cout << "\\    Y    /\\  ___/|  |_|  |_(  <_> )  \\        (  <_> )  | \\/  |__/ /_/ | " << endl;
+	cout << " \\___|_  /  \\___  >____/____/\\____/    \\__/\\  / \\____/|__|  |____/\\____ | " << endl;
+	cout << "       \\/       \\/                          \\/                         \\/ " << endl;
+	cout << "           ______________ ___ .___  _________ .___  _________             " << endl;
+	cout << "           \\__    ___/   |   \\|   |/   _____/ |   |/   _____/             " << endl;
+	cout << "             |    | /    ~    \\   |\\_____  \\  |   |\\_____  \\              " << endl;
+	cout << "             |    | \\    Y    /   |/        \\ |   |/        \\             " << endl;
+	cout << "             |____|  \\___|_  /|___/_______  / |___/_______  /             " << endl;
+	cout << "                           \\/             \\/              \\/              " << endl;
+	cout << "              ________  _______________   ____.___ _______                " << endl;
+	cout << "              \\______ \\ \\_   _____/\\   \\ /   /|   |\\      \\               " << endl;
+	cout << "               |    |  \\ |    __)_  \\   Y   / |   |/   |   \\              " << endl;
+	cout << "               |    `   \\|        \\  \\     /  |   /    |    \\             " << endl;
+	cout << "              /_______  /_______  /   \\___/   |___\\____|__  /             " << endl;
+	cout << "                      \\/        \\/                        \\/   " << endl;
 }
 
 void PlayGame()
